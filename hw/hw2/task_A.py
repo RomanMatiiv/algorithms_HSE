8a8e173fe7c7c63e4c121289f1888d0ba13b6496
RomanMatiiv <matiiv.r.a@gmail.com>
2020-04-19 23:11:39 +0300

    hw 2 task A done

--- hw/hw2/task_A.py
+++ hw/hw2/task_A.py
@@ -9,7 +9,8 @@
 После выполнения каждой команды программа должна вывести одну строчку.
 Возможные команды для программы:
     * push n
-        Добавить в стек число n (значение n задается после команды). Программа должна вывести ok.
+        Добавить в стек число n (значение n задается после команды).
+        Программа должна вывести ok.
     * pop
         Удалить из стека последний элемент. Программа должна вывести его значение.
     * back
@@ -26,3 +27,47 @@
 Если во входных данных встречается операция back или pop, и при этом стек пуст,
 то программа должна вместо числового значения вывести строку error.
 """
+import sys
+from hw.data_structure.stack import StackWithClear
+
+
+if __name__ == "__main__":
+    stack = StackWithClear()
+
+    for line in sys.stdin:
+        line = line.strip()
+        line = line.split(" ")
+
+        command = line[0]
+
+        if command == "push":
+            value = line[1]
+            stack.push(value)
+            print("ok")
+
+        elif command == "pop":
+            try:
+                val = stack.pop()
+            except AttributeError:
+                print("error")
+            else:
+                print(val)
+
+        elif command == "back":
+            try:
+                val = stack.back()
+            except AttributeError:
+                print("error")
+            else:
+                print(val)
+
+        elif command == "size":
+            size = stack.size
+
+        elif command == "clear":
+            stack.clear()
+            print("ok")
+
+        elif command == "exit":
+            print("bye")
+            break
