458c772504f105c4a5638d06e1c5f4b5e3c19786
RomanMatiiv <matiiv.r.a@gmail.com>
2020-04-19 21:18:34 +0300

    implement stack on linked list

---
+++ hw/data_structure/stack.py
@@ -0,0 +1,28 @@
+from hw.data_structure.linked_list import LinkedList
+
+
+class Stack:
+    """
+    Структура данных стек
+    Реализованная через двух связный список
+    """
+    def __init__(self):
+        self._stack = LinkedList()
+        self.size = 0
+
+    def push(self, value):
+        """Добавить в стек число value"""
+        self._stack.insert_head(value)
+        self.size += 1
+
+    def pop(self):
+        """Извлечь верхний элемент из стека"""
+        back_value = self._stack.pop_head()
+        self.size -= 1
+
+        return back_value
+
+    def back(self):
+        """Вернуть верхний элемент стека не извлекая его """
+        return self._stack.get_head()
+
