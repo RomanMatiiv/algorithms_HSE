bd12d11d851e12fe2a6010737178951950a09d16
RomanMatiiv <matiiv.r.a@gmail.com>
2020-04-20 01:04:32 +0300

    hw2 task B done

---
+++ hw/hw2/task_B.py
@@ -0,0 +1,35 @@
+"""
+Задача B. Правильная скобочная последовательность
+
+Программа получает на вход последовательность из скобок
+(открывающихся и закрывающихся скобок трех видов).
+Длина последовательности не превышает 255 символов.
+Последовательность не содержит пробелов
+(но после последнего символа могут идти пробелы и переходы на новую строку).
+"""
+from hw.data_structure.stack import Stack
+
+OPENING_BRACKETS = ["(", "[", "{"]
+
+BRACKETS_MAPPING = {")": "(",
+                    "]": "[",
+                    "}": "{"}
+
+inp = input().strip()
+stack = Stack()
+result = None
+
+for bracket in inp:
+    if bracket in OPENING_BRACKETS:
+        stack.push(bracket)
+    else:
+        if BRACKETS_MAPPING[bracket] != stack.pop():
+            result = "no"
+            break
+
+if stack.size == 0:
+    result = "yes"
+else:
+    result = "no"
+
+print(result)
