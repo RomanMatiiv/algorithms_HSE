8273bd1c1d806e8c31aaa18fd06e96851c5d6d41
RomanMatiiv <matiiv.r.a@gmail.com>
2020-04-28 00:37:56 +0300

    write stack with min()

--- hw/data_structure/stack.py
+++ hw/data_structure/stack.py
@@ -30,3 +30,35 @@ class Stack:
         """Вернуть верхний элемент стека не извлекая его """
         return self._stack.get_head()
 
+
+class StackMin:
+    def __init__(self):
+        self._stack = Stack()
+        self._stack_min = Stack()
+        self.size = 0
+
+    def push(self, value):
+        self._stack.push(value)
+        self.size += 1
+
+        cur_min = self.min()
+
+        if cur_min is None:
+            self._stack_min.push(value)
+        else:
+            if value < cur_min:
+                self._stack_min.push(value)
+            else:
+                self._stack_min.push(cur_min)
+
+    def pop(self):
+        if self._stack.back() is not None:
+            self.size -= 1
+        self._stack_min.pop()
+        return self._stack.pop()
+
+    def back(self):
+        return self._stack.back()
+
+    def min(self):
+        return self._stack_min.back()
