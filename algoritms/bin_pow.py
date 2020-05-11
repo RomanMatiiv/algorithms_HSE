5e0aca746226c9dcb4934526259e940f48e8c468
RomanMatiiv <matiiv.r.a@gmail.com>
2020-05-12 02:29:50 +0300

    refactoring

--- algoritms/bin_pow.py
+++ algoritms/bin_pow.py
@@ -11,6 +11,7 @@ def bin_pow(a, n):
     elif n == 1:
         return a
     elif n % 2 == 0:
-        return bin_pow(a**2, n/2)
+        reduced_power = bin_pow(a, n/2)
+        return reduced_power * reduced_power
     elif n % 2 != 0:
         return a * bin_pow(a, n-1)
