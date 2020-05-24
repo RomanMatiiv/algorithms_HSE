cdaa2c2e60144c911c5bf2e39a9101efbc7bd5b1
RomanMatiiv <matiiv.r.a@gmail.com>
2020-05-24 16:49:29 +0300

    hw4 task G done

--- hw/hw4/task_G.py
+++ hw/hw4/task_G.py
@@ -3,3 +3,13 @@
 
 Вам необходимо реализовать алгоритм сортировки merge-sort.
 """
+from algoritms.sorting.merge_sort import merge_sort
+
+if __name__ == '__main__':
+    _ = input()
+    arr = [int(i) for i in input().strip().split()]
+
+    sorted_arr = merge_sort(arr)
+
+    for val in sorted_arr:
+        print(val, end=" ")
