0f1c37e35366c63c01257a9b6953771585d42b63
RomanMatiiv <matiiv.r.a@gmail.com>
2020-05-24 20:16:34 +0300

    write test for counting sort

---
+++ algoritms/tests/test_counting_sort.py
@@ -0,0 +1,29 @@
+from algoritms.sorting.counting_sort import counting_sort
+
+
+def test_on_zero_to_100():
+    arr = [3, 1, 2, 0, 4, 30, 44, 2, 1, 4]
+
+    true_result = sorted(arr)
+    result = counting_sort(arr)
+
+    assert result == true_result
+
+
+def test_with_minus_value():
+    arr = [3, -2, 1, 34, 2, -8]
+    true_result = sorted(arr)
+    result = counting_sort(arr)
+    assert result == true_result
+
+    arr = [-3, -1, -2, -9]
+    true_result = sorted(arr)
+    result = counting_sort(arr)
+    assert result == true_result
+
+
+def test_on_empty_arr():
+    arr = []
+    true_result = sorted(arr)
+    result = counting_sort(arr)
+    assert result == true_result
