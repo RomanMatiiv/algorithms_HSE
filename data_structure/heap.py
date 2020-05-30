e3b472d4dedc8e07ce1f6902db8d8397fe4fd2c0
RomanMatiiv <matiiv.r.a@gmail.com>
2020-05-31 02:17:48 +0300

    wirite interface for MinHeap and MaxHeap

---
+++ data_structure/heap.py
@@ -0,0 +1,45 @@
+
+class MinHeap:
+    def __int__(self):
+        raise NotImplemented
+
+    def push(self, value) -> None:
+        raise NotImplemented
+
+    def pop_min(self):
+        raise NotImplemented
+
+    def min(self):
+        raise NotImplemented
+
+    def heapify(self) -> None:
+        raise NotImplemented
+
+    def _sift_up(self) -> None:
+        raise NotImplemented
+
+    def _sift_down(self) -> None:
+        raise NotImplemented
+
+
+class MaxHeap:
+    def __int__(self):
+        raise NotImplemented
+
+    def push(self, value) -> None:
+        raise NotImplemented
+
+    def pop_max(self):
+        raise NotImplemented
+
+    def max(self):
+        raise NotImplemented
+
+    def heapify(self) -> None:
+        raise NotImplemented
+
+    def _sift_up(self) -> None:
+        raise NotImplemented
+
+    def _sift_down(self) -> None:
+        raise NotImplemented
