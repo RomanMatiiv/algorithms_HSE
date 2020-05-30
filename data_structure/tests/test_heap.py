877b894a59c0e6e90bdec53477ca360b7b551704
RomanMatiiv <matiiv.r.a@gmail.com>
2020-05-31 02:19:05 +0300

    write test for MinHeap and MaxHeap

---
+++ data_structure/tests/test_heap.py
@@ -0,0 +1,104 @@
+from data_structure.heap import MaxHeap
+from data_structure.heap import MinHeap
+
+
+def test_min_heap_push():
+    heap_min = MinHeap()
+    heap_min.push(1)
+    heap_min.push(2)
+
+
+def test_min_heap_min():
+    heap_min = MinHeap()
+
+    try:
+        heap_min.min()
+    except:
+        pass
+    else:
+        raise AssertionError
+
+    heap_min.push(1)
+    assert heap_min.min() == 1
+
+    heap_min.push(0)
+    assert heap_min.min() == 0
+
+    heap_min.push(3)
+    assert heap_min.min() == 0
+
+
+def test_min_heap_pop():
+    heap_min = MinHeap()
+
+    try:
+        heap_min.pop_min()
+    except:
+        pass
+    else:
+        raise AssertionError
+
+    heap_min.push(1)
+    heap_min.push(3)
+    assert heap_min.pop_min() == 1
+    assert heap_min.min() == 3
+
+    assert heap_min.pop_min() == 3
+
+    try:
+        heap_min.pop_min()
+    except:
+        pass
+    else:
+        raise AssertionError
+
+
+def test_max_heap_push():
+    heap_max = MaxHeap()
+    heap_max.push(1)
+    heap_max.push(2)
+
+
+def test_max_heap_max():
+    heap_max = MaxHeap()
+
+    try:
+        heap_max.max()
+    except:
+        pass
+    else:
+        raise AssertionError
+
+    heap_max.push(1)
+    assert heap_max.max() == 1
+
+    heap_max.push(2)
+    assert heap_max.max() == 2
+
+    heap_max.push(0)
+    assert heap_max.max() == 2
+
+
+def test_max_heap_pop():
+    heap_max = MaxHeap()
+
+    try:
+        heap_max.pop_max()
+    except:
+        pass
+    else:
+        raise AssertionError
+
+    heap_max.push(1)
+    heap_max.push(3)
+    assert heap_max.pop_max() == 3
+    assert heap_max.max() == 1
+
+    assert heap_max.pop_max() == 1
+
+    try:
+        heap_max.pop_max()
+    except:
+        pass
+    else:
+        raise AssertionError
