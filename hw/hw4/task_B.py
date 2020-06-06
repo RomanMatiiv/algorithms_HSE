7f2349a163a99353e06c4deb5ff3be29ecde120a
RomanMatiiv <matiiv.r.a@gmail.com>
2020-06-06 13:13:47 +0300

    task B hw 4 done

--- hw/hw4/task_B.py
+++ hw/hw4/task_B.py
@@ -1,3 +1,16 @@
 """
-Задача
+Задача B
+
+Отсортировать массив с помощью пирамидальной сортировки
 """
+from algoritms.sorting.heap_sort import heap_sort
+
+
+if __name__ == '__main__':
+    _ = input()
+    arr = [int(i) for i in input().strip().split()]
+
+    sorted_arr = heap_sort(arr)
+
+    for val in sorted_arr:
+        print(val, end=" ")
