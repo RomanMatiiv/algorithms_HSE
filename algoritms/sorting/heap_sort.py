428dfe9cb6e48fa2f7923678e5393255c6dbb2f2
RomanMatiiv <matiiv.r.a@gmail.com>
2020-06-06 13:13:23 +0300

    write heap sort

--- algoritms/sorting/heap_sort.py
+++ algoritms/sorting/heap_sort.py
@@ -1,4 +1,13 @@
+from data_structure.heap import MinHeap
 
 
 def heap_sort(array: list):
-    raise NotImplemented
+    sorted_array = []
+    heap = MinHeap(array)
+
+    while len(heap) > 0:
+        cur_min_val = heap.pop_min()
+        sorted_array.append(cur_min_val)
+
+    return sorted_array
+
