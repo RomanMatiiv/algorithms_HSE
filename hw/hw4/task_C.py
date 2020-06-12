40afde2705303fa63f89f3862f73fd101272e93e
RomanMatiiv <matiiv.r.a@gmail.com>
2020-06-12 20:34:01 +0300

    task С hw 4 done

--- hw/hw4/task_C.py
+++ hw/hw4/task_C.py
@@ -1,3 +1,27 @@
 """
-Задача
+Задача C
+В данном списке из n ≤ 10^5 целых чисел найдите три числа,
+произведение которых максимально.
+
+Решение должно иметь сложность O(n), где n - размер списка.
+
+Выведите три искомых числа в любом порядке.
 """
+from algoritms.sorting.counting_sort import counting_sort
+
+if __name__ == '__main__':
+    array = [int(i) for i in input().strip().split()]
+
+    sorted_arr = counting_sort(array)
+
+    min_vals = sorted_arr[:3]
+    max_vals = sorted_arr[-3:]
+
+    if max_vals[0]*max_vals[1]*max_vals[2] > min_vals[0]*min_vals[1]*max_vals[2]:
+        print(max_vals[0], max_vals[1], max_vals[2])
+    else:
+        print(min_vals[0], min_vals[1], max_vals[2])
+
+
+
+
