4b31044cb0afbaa04114f3be289a9bd57ddaeda8
RomanMatiiv <matiiv.r.a@gmail.com>
2020-06-24 18:02:07 +0300

    write interface for HashTable

---
+++ data_structure/hash_table.py
@@ -0,0 +1,17 @@
+from typing import NoReturn
+
+
+class HashTable:
+    def __init__(self):
+        self.size = None
+        self.alpha = None
+        raise NotImplemented
+
+    def insert(self, key, val) -> NoReturn:
+        raise NotImplemented
+
+    def remove(self, key) -> NoReturn:
+        raise NotImplemented
+
+    def has(self, key) -> NoReturn:
+        raise NotImplemented
