b7e7e6d00c5a3f1a888739d7128a7b5b7f2e37a4
RomanMatiiv <matiiv.r.a@gmail.com>
2020-07-05 23:56:07 +0300

    refactoring

--- data_structure/hash_table.py
+++ data_structure/hash_table.py
@@ -1,5 +1,6 @@
 from data_structure.linked_list import DoublyLinkedList
 
+
 class Node:
     __slots__ = ['key', 'data']
 
@@ -9,6 +10,10 @@ class Node:
 
 
 class HashTable:
+    """
+    Реализация хеш таблицы
+    Метод разрешения коллизий: метод цепочек
+    """
     def __init__(self, size=8, hash_func=hash):
         self.size = size
         self.alpha = None
@@ -26,60 +31,42 @@ class HashTable:
 
         Returns: None
         """
-        # TODO отрефакторить а то слишком запутанно (cell node)
-        cell_index = self.hash_func(key) % self.size
-
+        linked_list_index = self.hash_func(key) % self.size
         node = Node(key, val)
 
         # без коллизий
-        if self.table[cell_index] is None:
-            self.table[cell_index] = DoublyLinkedList()
-            self.table[cell_index].insert_head(node)
+        if self.table[linked_list_index] is None:
+            self.table[linked_list_index] = DoublyLinkedList()
+            self.table[linked_list_index].insert_head(node)
         # коллизия
         else:
-            self.table[cell_index].insert_tail(node)
+            self.table[linked_list_index].insert_tail(node)
 
         return None
 
     def remove(self, key) -> None:
-        # TODO добавить в DoublyLinkedList возможность удаления узла
         raise NotImplemented
 
     def has(self, key) -> bool:
-        # TODO отрефакторить а то слишком запутанно (cell node)
+        linked_list_index = self.hash_func(key) % self.size
+        linked_list = self.table[linked_list_index]
 
-        # TODO переписать добавив возможность итерирования по DoublyLinkedList
-        cell_index = self.hash_func(key) % self.size
-        cell = self.table[cell_index]
-
-        if cell is not None:
-            cur_node = cell._head
-
-            while cur_node is not None:
-                if cur_node.value.key == key:
+        if linked_list is not None:
+            for cur_node in linked_list:
+                if cur_node.key == key:
                     return True
-                else:
-                    cur_node = cur_node.next_node
-
         return False
 
     def get(self, key):
-        # TODO отрефакторить а то слишком запутанно (cell node)
-        cell_index = self.hash_func(key) % self.size
-        cell = self.table[cell_index]
+        linked_list_index = self.hash_func(key) % self.size
+        linked_list = self.table[linked_list_index]
 
-        if cell is not None:
-            cur_node = cell._head
-
-            while cur_node is not None:
-                if cur_node.value.key == key:
-                    return cur_node.value.data
-                else:
-                    cur_node = cur_node.next_node
-
+        if linked_list is not None:
+            for cur_node in linked_list:
+                if cur_node.key == key:
+                    return cur_node.data
         else:
             raise KeyError
-
 
     def __len__(self):
         raise NotImplemented
